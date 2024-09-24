3d247aa5d8a8512801ca715771d16fc62e23100b
RemiLisciandra <rlisciandra@gmail.com>
2024-09-24 11:36:20 +0200

    First commit

---
+++ lib/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuORauu9RZ9ly5LUA4M-D1GIiIpu6iawc",
+  authDomain: "healthdonalds-remi-lcd.firebaseapp.com",
+  projectId: "healthdonalds-remi-lcd",
+  storageBucket: "healthdonalds-remi-lcd.appspot.com",
+  messagingSenderId: "49318259700",
+  appId: "1:49318259700:web:765e7eab295c0108e067a1",
+  measurementId: "G-N0TYLS93TS",
+};
+
+export const app = initializeApp(firebaseConfig);
